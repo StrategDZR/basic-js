fa7f3d2f97bcaf3a299de66ad9951f3ac552a490
Alexey Shein <alexey.shein@revelsystems.com>
2020-02-16 17:11:40 +0300

    count-cats.js solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+    let counter = 0;
+    arr.forEach(item => {
+        item.forEach(item2 => {
+            if (item2 === '^^'){
+                counter++;
+            }
+        })
+    })
+    return counter;
 };

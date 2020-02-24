a3d758abb8d6fd7bcb8dadb41eee3945ca475ce5
Alexey Shein <alexey.shein@revelsystems.com>
2020-02-24 11:30:11 +0300

    fix: recursive-depth.js updated

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,5 +1,12 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
-
+        let count = 1
+        arr.forEach(el => {
+            if (Array.isArray(el)) {
+                let tmp = this.calculateDepth(el) + 1
+                if (tmp > count) count = tmp
+            }
+        });
+        return count
     }
 };
